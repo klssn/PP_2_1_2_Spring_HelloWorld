53028addf249953984759ce42d74dc08e262f496
klssn <klsseneva@gmail.com>
2023-07-25 13:51:55 +0900

    test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        //System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        /*HelloWorld bean = applicationContext.getBean(HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);*/
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        /*Cat bean3 = applicationContext.getBean(Cat.class);
+        Cat bean4 = applicationContext.getBean(Cat.class);*/
+
+        System.out.println(bean == bean2);
+        System.out.println(bean3 == bean4);
     }
 }
