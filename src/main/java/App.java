830f4b804b34f5fb8cb7b98d05524446861a4216
klssn <klsseneva@gmail.com>
2023-07-25 20:56:48 +0900

    test2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,11 +10,15 @@ public class App {
         HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         /*HelloWorld bean = applicationContext.getBean(HelloWorld.class);
         HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);*/
+        /*HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);*/
 
         Cat bean3 = (Cat) applicationContext.getBean("cat");
         Cat bean4 = (Cat) applicationContext.getBean("cat");
         /*Cat bean3 = applicationContext.getBean(Cat.class);
         Cat bean4 = applicationContext.getBean(Cat.class);*/
+        /*Cat bean3 = applicationContext.getBean("cat", Cat.class);
+        Cat bean4 = applicationContext.getBean("cat", Cat.class);*/
 
         System.out.println(bean == bean2);
         System.out.println(bean3 == bean4);
